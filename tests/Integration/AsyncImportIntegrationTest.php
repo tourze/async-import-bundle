3fae6a62554e300d230103489e76ee9e43cf0000
github-ci <you@example.com>
2025-06-11 11:25:13 +0800

    update

--- tests/Integration/AsyncImportIntegrationTest.php
+++ tests/Integration/AsyncImportIntegrationTest.php
@@ -11,7 +11,10 @@ class AsyncImportIntegrationTest extends KernelTestCase
 {
     protected static function createKernel(array $options = []): KernelInterface
     {
-        return new IntegrationTestKernel('test', true, [
+        $env = $options['environment'] ?? $_ENV['APP_ENV'] ?? $_SERVER['APP_ENV'] ?? 'test';
+        $debug = $options['debug'] ?? $_ENV['APP_DEBUG'] ?? $_SERVER['APP_DEBUG'] ?? true;
+
+        return new IntegrationTestKernel($env, $debug, [
             AsyncImportBundle::class => ['all' => true],
         ]);
     }
